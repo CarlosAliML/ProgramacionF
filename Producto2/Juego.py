c0a7d0a13c9c2e64d61744fd87015d39443fe37b
Carlos Medina <cali.medina5423@gmail.com>
2015-02-11 18:02:54 -0700

    Creando juegos

---
+++ Producto2/Juego.py
@@ -0,0 +1,21 @@
+# Hello World in Python
+import time
+while True:
+	print "Hola! Tratare de adivinar un numero. Piensa un numero entre 1 y 10."
+time.sleep(5)
+import time
+while True:
+	print "Ahora multiplicalo por 9."
+time.sleep(5)
+import time
+while True:
+	print "Si el numero tiene 2 digitos, sumalos entre si: Ej. 36 -> 3+6=9. Si tu numero tiene un solo digito, sumale 0."
+time.sleep(5)
+import time
+while True:
+	print "Al numero resultante sumale 4."
+time.sleep(10)
+import time
+while True:
+	print "Muy bien. El resultado es 13 :3 "
+
